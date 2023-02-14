bc95625f551512fa8b54cc76e59d8e2671e0b338
Yam Dangar <yamdangar@gmail.com>
2023-02-14 15:37:53 +1100

    dashboard created/ sidebar

--- src/pages/layout/AdminLayout.js
+++ src/pages/layout/AdminLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 
@@ -7,9 +8,67 @@ export const AdminLayout = ({ children }) => {
   return (
     <div>
       <Header />
-      <Container>
-        <main>{children}</main>
-      </Container>
+
+      <main className="main">
+        <Container fluid>
+          <Row>
+            <Col xs="3" className="side-bar bg-dark text-light">
+              <div className="mt-5">
+                <div className="text-center fw-bolder">Admin Menu</div>
+              </div>
+              <hr />
+              <div className="sidebar-menu">
+                <ul>
+                  <li>
+                    <Link to="/dashboard">
+                      <i class="fa-solid fa-gauge"></i>
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/catalog">
+                      <i class="fa-sharp fa-solid fa-sitemap"></i>
+                      Catalog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/products">
+                      <i class="fa-brands fa-product-hunt"></i>
+                      Products
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/payment">
+                      <i class="fa-solid fa-credit-card"></i>
+                      Payment Methods
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/orders">
+                      <i class="fa-solid fa-truck"></i>
+                      Orders
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/customers">
+                      <i class="fa-solid fa-credit-card"></i>
+                      Customers
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/settings">
+                      <i class="fa-sharp fa-solid fa-gears"></i>
+                      Settings
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+            <Col>{children}</Col>
+          </Row>
+        </Container>
+      </main>
+
       <Footer />
     </div>
   );
--- src/pages/layout/Header.js
+++ src/pages/layout/Header.js
@@ -1,9 +1,19 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Navigate } from "react-router-dom";
+import { requestSuccess } from "../login/authSlice";
 
 export const Header = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+
+  const handleOnLogout = () => {
+    dispatch(requestSuccess({}));
+    Navigate("/");
+  };
+
   return (
     <Navbar bg="warning" expand="md">
       <Container>
@@ -13,12 +23,28 @@ export const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Link to="/" className="nav-link">
-              Login
-            </Link>
-            <Link to="/register" className="nav-link">
-              Sign Up
-            </Link>
+            {user?._id ? (
+              <>
+                <Link to="/" className="nav-link">
+                  <i class="fa-solid fa-bell"></i>
+                </Link>
+                <Link to="/register" className="nav-link">
+                  <i class="fa-solid fa-pen-to-square"></i>
+                </Link>
+                <Link to="/" className="nav-link" onClick={handleOnLogout}>
+                  <i class="fa-solid fa-right-from-bracket"></i>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link to="/" className="nav-link">
+                  <i class="fa-solid fa-right-from-bracket"></i>
+                </Link>
+                <Link to="/register" className="nav-link">
+                  <i class="fa-solid fa-pen-to-square"></i>
+                </Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
